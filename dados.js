25fb3b705af64165c4e328053e364e918d76029a
RenanHoffmann <renan-hoffmann1@hotmail.com>
2024-09-06 21:04:55 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,17 @@
+let filmes = [
+    {
+        titulo: "O Senhor dos Anéis - A Sociedade do Anel",
+        descricao: "Em uma terra mítica chamada Terra-média, um hobbit chamado Frodo Bolseiro herda um anel mágico de grande poder. Descobrindo que se trata do Um Anel, forjado pelo Senhor das Trevas Sauron para dominar o mundo, Frodo embarca em uma perigosa jornada para destruí-lo no Monte do Destino. Acompanhado por uma sociedade formada por diversos seres, como elfos, anões, humanos e outros hobbits, Frodo enfrenta criaturas das sombras, tentações do anel e a própria divisão da sociedade. A jornada se torna cada vez mais desafiadora, testando a coragem, a amizade e a esperança de todos.",
+        site: "https://pt.wikipedia.org/wiki/O_Senhor_dos_An%C3%A9is#:~:text:O_Senhor_dos_An%C3%A9is% (The_Lord_of_the_Rings)"
+    },
+    {
+        titulo: "Madrugada dos Mortos",
+        descricao: "Em um shopping center, um grupo de sobreviventes luta para escapar de um apocalipse zumbi que assola o mundo. Enquanto tentam se proteger dos mortos-vivos, eles enfrentam dilemas morais e conflitos internos que os colocam à prova.",
+        site: "https://pt.wikipedia.org/wiki/Dawn_of_the_Dead_(2004)" // Substitua por um link real
+    },
+    {
+        titulo: "A Lagoa Azul",
+        descricao: "Dois jovens náufragos crescem juntos em uma ilha paradisíaca e descobrem o amor e a vida.",
+        site: "https://pt.wikipedia.org/wiki/Lagoa_Azul_(Isl%C3%A2ndia)#:~:text=A%20Lagoa%20Azul%20%28em%20island%C3%AAs%3A%20%22Bl%C3%A1a%20l%C3%B3ni%C3%B0%22%3B%20PRON%C3%9ANCIA%29,%C3%A1gua%20em%205%20000%20m%202%20de%20%C3%A1rea." // Substitua por um link real
+    }
+];
